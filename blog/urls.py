940e9c87927bdc89f1cdf1477f3a28d2ec746e30
Ruhul Amin <ruhulinfo50@gmail.com>
2020-06-09 17:10:10 +0600

    added CRUD functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
 from . import views
+from . views import BlogCreateView,BlogUpdate,Blogdetails,BlogList,BlogDelete
 urlpatterns = [
     
-    path('', views.index, name = 'index'),
-    path('post/<int:post_id>/',views.post,name='post')
+    path('', BlogList.as_view(), name = 'index'),
+    path('post/<int:pk>/',Blogdetails.as_view(),name='post'),
+    path('post/new/', BlogCreateView.as_view(),name = 'new_post'),
+    path('post/<int:pk>/edit/',BlogUpdate.as_view(),name='edit_post'),
+    path('post/<int:pk>/delete/',BlogDelete.as_view(),name='delete_post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,47 @@
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView,DetailView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
 from .models import Post
-def index(request):
-    posts = Post.objects.all()
-    contex = {
-        'posts':posts
-    }
-    return render(request,'blog/index.html',contex)
+
+class BlogList(ListView):
+    model = Post 
+    template_name = 'blog/index.html'
+
+# def index(request):
+#     posts = Post.objects.all()
+#     contex = {
+#         'posts':posts
+#     }
+#     return render(request,'blog/index.html',contex)
 
 
-def post(request,post_id):
-    posts = get_object_or_404(Post,pk=post_id)
-    contex = {
-        'posts':posts
-    }
-    return render(request,'blog/post_details.html',contex)
+# def post(request,post_id):
+#     posts = get_object_or_404(Post,Post.pk)
+#     contex = {
+#         'posts':posts
+#     }
+#     return render(request,'blog/post_details.html',contex)
+
+class Blogdetails(DetailView):
+    model = Post 
+    template_name = 'blog/post_details.html'
+    # contex_object-name = 'indiv_post'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'blog/new_post.html'
+    fields = '__all__'
+
+class BlogUpdate(UpdateView):
+    model = Post
+    template_name = 'blog/edit_post.html'
+    fields  = ['title','body']
+
+
+class BlogDelete(DeleteView):
+    model = Post 
+    template_name = 'blog/delete_post.html'
+    success_url = reverse_lazy('index')
